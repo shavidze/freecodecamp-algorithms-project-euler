1ff99f9cd443f7bb0584f9a33701392b20f4a10b
sabashavidze <sabashavidze@gmail.com>
2020-02-20 05:21:46 -0800

    power of digit sum

--- powerOfDigitSum.js
+++ powerOfDigitSum.js
@@ -1,23 +1,49 @@
 function powerDigitSum(exponent) {
   // Good luck!
   let divs = new Array(exponent).fill(-1);
+
   var add = (x, y) => {
+
+    x = String(x).split("").reverse().join("");
+    y = String(y).split("").reverse().join("");
+
     if (x.length > y.length) {
       [x, y] = [y, x];
     }
+
     let n1 = x.length;
     let n2 = y.length;
-    x = x.reverse();
-  };
-  numberOfDivisors(exponent);
-  for (let x of divs) {
-    if (x === -1) continue;
-    console.log(x);
-  }
-  let sumOfDigits = 0;
+    let carry = 0;
+    let str = ""
+    for (let i = 0; i < n1; i++) {
+      let sum = parseInt(x[i]) + parseInt(y[i]) + carry;
+      str = str.concat(String(Math.floor(sum % 10)));
+      carry = Math.floor(sum / 10);
+    }
 
-  return sumOfDigits;
+    for (let i = n1; i < n2; i++) {
+      let sum = parseInt(y[i]) + carry;
+      str = str.concat(String(Math.floor(sum % 10)));
+      carry = Math.floor(sum / 10);
+    }
+    if (carry) {
+      str = str.concat(String(carry));
+    }
+    return str.split("").reverse().join("");
+  };
+  let answer = "0";
+  for (let i = 1; i <= exponent; i++) {
+    if (i === 1) {
+      answer = "2";
+      continue;
+    }
+    answer = add(answer, answer);
+  }
+  let sm = 0;
+  for (let i = 0; i < answer.length; i++) {
+    sm += parseInt(answer[i]);
+  }
+  return sm;
 }
 
-console.log(powerDigitSum(128));
-powerDigitSum(15);
+console.log(powerDigitSum(15));
