b0addfad76f31cdeb8d5a0c3f4db9be50ca02c40
shavidze <sabashavidze@gmail.com>
2020-03-19 08:06:25 -0700

    sum of digits n!

--- factorial-digit-sum.js
+++ factorial-digit-sum.js
@@ -1,29 +1,53 @@
 function sumFactorialDigits(n) {
   // Good luck!
-  let logSums = 0;
-  let multiply = (xvector, y) => {
-    let carry = 0;
-    let sz = xvector.length;
-    for (let i = 0; i < sz; i++) {
-      let res = carry + xvector[i] * y;
-      xvector[i] = res % 10;
-      carry = Math.floor(res / 10);
+
+  var mutliply = (x, y) => {
+    let res = new Array(x.length + y.length + 1).fill(0);
+    if (x.length === 0 || y.length === 0) return "0";
+    let n1 = x.length;
+    let n2 = y.length;
+    let iy = 0;
+    let ix = 0;
+    for (let i = n1 - 1; i >= 0; i--) {
+      let carry = 0;
+      let a = parseInt(x[i]);
+      iy = 0;
+      for (let j = n2 - 1; j >= 0; j--) {
+        let b = parseInt(y[j]);
+        let sum = a * b + res[iy + ix] + carry;
+        carry = Math.floor(sum / 10);
+        res[ix + iy] = sum % 10;
+        iy++;
+      }
+      if (carry > 0) {
+        res[ix + iy] += carry;
+      }
+      ix++;
     }
-    while (carry != 0) {
-      xvector.push(carry % 10);
-      carry = Math.floor(carry / 10);
+    let i = res.length - 1;
+    while (i >= 0 && res[i] === 0) {
+      i--;
     }
-    return xvector;
+    if (i === -1) {
+      return "0";
+    }
+    let ans = "";
+    while (i >= 0) {
+      ans += String(res[i]);
+      i--;
+    }
+    return ans;
   };
-  let vc = [];
-  vc[0] = 1;
-  for (let i = 1; i <= n; i++) {
-    vc = multiply(vc, i);
+
+  let answer2 = "1";
+  for (let i = 2; i <= n; i++) {
+    answer2 = mutliply(String(i), answer2);
   }
-  for (let i = 0; i < vc.length; i++) {
-    logSums += vc[i];
+  let digitsSums2 = 0;
+  for (let i = 0; i < answer2.length; i++) {
+    digitsSums2 += parseInt(answer2[i]);
   }
-  return logSums;
+  return digitsSums2;
 }
 
-console.log(sumFactorialDigits(100));
+sumFactorialDigits(100);
